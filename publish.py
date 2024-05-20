a3d657e50e661999a548d0718baadeab98b32d72
joseantunesNAUAI <jose.antunes@nauai.pt>
2024-05-20 13:17:08 +0100

    MES-590: Create MQTT client - each file (publish or subscribe) works and MQTT client that will only be able to send messages or receibe (not both)

--- publish.py
+++ publish.py
@@ -1,30 +1,24 @@
+import time
 import paho.mqtt.client as mqtt
 import json
 import datetime
 
 from connect import connect_mqtt
-from subscribe import subscribe_mqtt
 
-def on_connect(client, userdata, flags, rc):
-    print(f"Connected with result code {rc}")
+client_id = "iotconsole-d0d0f57f-f94b-4c46-95d5-a84bb43660cc"
+broker_url = "a3sdserc3gohpq-ats.iot.eu-central-1.amazonaws.com"
+ca_cert = "AmazonRootCA1.pem"
+certfile = "323d7c3fe3ed141225b1846da88ba2b9d587165ab60ac6965ff316d4203f0140-certificate.pem.crt"
+keyfile = "323d7c3fe3ed141225b1846da88ba2b9d587165ab60ac6965ff316d4203f0140-private.pem.key"
+topicSend = "MASILVA/CRK/PROTOCOL_COUNT_V0/GTW"
 
-def publish_mqtt(client, topic, message):
-    client.publish(topic=topic, payload=message)
+client = connect_mqtt(client_id, broker_url, ca_cert, certfile, keyfile)
 
-def main():
-    client_id = "iotconsole-d0d0f57f-f94b-4c46-95d5-a84bb43660cc"
-    broker_url = "a3sdserc3gohpq-ats.iot.eu-central-1.amazonaws.com"
-    ca_cert = "AmazonRootCA1.pem"
-    certfile = "323d7c3fe3ed141225b1846da88ba2b9d587165ab60ac6965ff316d4203f0140-certificate.pem.crt"
-    keyfile = "323d7c3fe3ed141225b1846da88ba2b9d587165ab60ac6965ff316d4203f0140-private.pem.key"
-    topicReceive = "MASILVA/CRK/PROTOCOL_COUNT_V0/BE"
-    topicSend = "MASILVA/CRK/PROTOCOL_COUNT_V0/GTW"
-
-
+def send_message_mqtt(topicSend, message):
     messageInput = input("Enter your message: ")
-
     if messageInput == "exit":
         print("bye!")
+        client.disconnect()
         exit()
     else: 
         message = json.dumps(
@@ -33,12 +27,11 @@ def main():
                 "date_time": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             }
         )
+        client.publish(topicSend, message)
+    
 
-    client = connect_mqtt(client_id, broker_url, ca_cert, certfile, keyfile)
-    client.on_connect = on_connect
-    client.connect(broker_url, 8883)
-    client.publish(topicSend, message)
-    subscribe_mqtt(client, topicReceive)
+def publish_mqtt(client, topicSend):
+    while True:
+        send_message_mqtt(topicSend, client)
 
-if __name__ == "__main__":
-    main()
+publish_mqtt(client, topicSend)
--- subscribe.py
+++ subscribe.py
@@ -3,39 +3,24 @@ import time
 import json
 import datetime
 
+from connect import connect_mqtt
+
+client_id = "iotconsole-d0d0f57f-f94b-4c46-95d5-a84bb43660cc"
+broker_url = "a3sdserc3gohpq-ats.iot.eu-central-1.amazonaws.com"
+ca_cert = "AmazonRootCA1.pem"
+certfile = "323d7c3fe3ed141225b1846da88ba2b9d587165ab60ac6965ff316d4203f0140-certificate.pem.crt"
+keyfile = "323d7c3fe3ed141225b1846da88ba2b9d587165ab60ac6965ff316d4203f0140-private.pem.key"
+topicReceive = "MASILVA/CRK/PROTOCOL_COUNT_V0/BE"
+
 def on_message(client, userdata, msg):
     print(f"Received message: topic={msg.topic}, payload={msg.payload.decode()}")
-    messageInput = input("Enter your message: ")
-
-    if messageInput == "exit":
-        client.on_unsubscribe = on_unsubscribe
-        print("bye!")
-        exit()
-    else: 
-        message = json.dumps(
-            {
-                "message": messageInput,
-                "date_time": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            }
-        )
-
-    topicSend = "MASILVA/CRK/PROTOCOL_COUNT_V0/GTW"
-    message = json.dumps(
-        {
-            "message": messageInput,
-            "date_time": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        }
-    )
-    client.publish(topicSend, message)
-    print(f"Message sent. Waiting for some response...")
-
-def on_unsubscribe(client, userdata, mid, reason_code_list, properties):
-    client.disconnect()
 
 def subscribe_mqtt(client, topic):
     client.subscribe(topic)
     client.on_message = on_message
     while True:
-        client.loop()
+        client.loop_forever()
         time.sleep(1)
-        
+
+client = connect_mqtt(client_id, broker_url, ca_cert, certfile, keyfile)
+subscribe_mqtt(client, topicReceive)
