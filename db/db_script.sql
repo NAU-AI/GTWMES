c8a24e8c921f2dfc2361b7ddc961731db0febe58
joseantunesNAUAI <jose.antunes@nauai.pt>
2024-05-21 18:13:25 +0100

    MES-600: Create GTW database

---
+++ db/db_script.sql
@@ -0,0 +1,48 @@
+-- Create table counting_equipment
+CREATE TABLE counting_equipment (
+    id SERIAL PRIMARY KEY,
+    code VARCHAR(20) NOT NULL,
+    equipment_status INTEGER,
+    p_timer_communication_cycle INTEGER
+);
+
+-- Create table equipment_output
+CREATE TABLE equipment_output (
+    id SERIAL PRIMARY KEY,
+    counting_equipment_id INTEGER REFERENCES counting_equipment(id),
+    code VARCHAR(20) NOT NULL
+);
+
+-- Create table production_order
+CREATE TABLE production_order (
+    id SERIAL PRIMARY KEY,
+    equipment_id INTEGER REFERENCES counting_equipment(id),
+    code VARCHAR(20) NOT NULL
+);
+
+-- Create table active_time
+CREATE TABLE active_time (
+    id SERIAL PRIMARY KEY,
+    equipment_id INTEGER REFERENCES counting_equipment(id),
+    active_time INTEGER,
+    registered_at TIMESTAMP
+);
+
+-- Create table alarm
+CREATE TABLE alarm (
+    id SERIAL PRIMARY KEY,
+    equipment_id INTEGER REFERENCES counting_equipment(id),
+    alarm_1 INTEGER,
+    alarm_2 INTEGER,
+    alarm_3 INTEGER,
+    alarm_4 INTEGER,
+    registered_at TIMESTAMP
+);
+
+-- Create table counter_record
+CREATE TABLE counter_record (
+    id SERIAL PRIMARY KEY,
+    equipment_output_id INTEGER REFERENCES equipment_output(id),
+    real_value INTEGER,
+    registered_at TIMESTAMP
+);
