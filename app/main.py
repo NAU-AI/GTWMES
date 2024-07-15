4b5bd7ecb91d826c97c8e33ef31fcf2e610f4647
joseantunesNAUAI <jose.antunes@nauai.pt>
2024-07-15 15:03:20 +0100

    MES-677: Create GTW exec with last code version

--- app/main.py
+++ app/main.py
@@ -6,16 +6,17 @@ from dotenv import load_dotenv
 load_dotenv() 
 
 from api.connect import connect_mqtt
-broker_url = os.getenv("broker_url")
-ca_cert = os.getenv("ca_cert")
-certfile = os.getenv("certfile")
-keyfile = os.getenv("keyfile")
+from variables import broker_url, ca_cert, certfile, keyfile, FIRST_RECONNECT_DELAY, RECONNECT_RATE, MAX_RECONNECT_DELAY
+broker_url = broker_url
+ca_cert = ca_cert
+certfile = certfile
+keyfile = keyfile
 
 import api.publishSubscriberMES
 
 def main():
     #MQTT client initialization
-    reconnect_count, reconnect_delay = 0, int(os.getenv("FIRST_RECONNECT_DELAY"))
+    reconnect_count, reconnect_delay = 0, int(FIRST_RECONNECT_DELAY)
 
     while True:
         logging.info("Connecting in %d seconds...", reconnect_delay)
@@ -28,8 +29,8 @@ def main():
         except Exception as err:
             logging.error("%s. Connection failed. Retrying...", err)
 
-        reconnect_delay *= int(os.getenv("RECONNECT_RATE"))
-        reconnect_delay = min(reconnect_delay, int(os.getenv("MAX_RECONNECT_DELAY")))
+        reconnect_delay *= int(RECONNECT_RATE)
+        reconnect_delay = min(reconnect_delay, int(MAX_RECONNECT_DELAY))
         
 
 if __name__ == '__main__':
