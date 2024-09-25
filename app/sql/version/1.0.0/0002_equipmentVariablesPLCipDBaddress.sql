245f607ab21bbd5c20646a0398c8624e74abb8de
joseantunesNAUAI <jose.antunes@nauai.pt>
2024-09-25 12:01:37 +0100

    MES-761: Database for PLC connect and data - create .sql script to update database

---
+++ app/sql/version/1.0.0/0002_equipmentVariablesPLCipDBaddress.sql
@@ -0,0 +1,18 @@
+ALTER TABLE counting_equipment
+ADD COLUMN plc_ip VARCHAR(20) NOT NULL DEFAULT '0';
+
+ALTER TABLE counting_equipment
+ADD COLUMN db_address VARCHAR(20) NOT NULL DEFAULT '';
+
+CREATE TABLE equipment_variable (
+    id SERIAL PRIMARY KEY,
+    equipment_id INTEGER NOT NULL,
+    name VARCHAR(20) NOT NULL,
+    offset_byte INTEGER NOT NULL,
+    offset_bit INTEGER NOT NULL,
+    FOREIGN KEY (equipment_id) REFERENCES counting_equipment(id)
+);
+
+INSERT INTO audit_script (run_date, process, version, schema)
+VALUES
+    (CURRENT_DATE, '0002_equipmentVariablesPLCipDBaddress.sql', '1.0.0', '1.0.0_0002');
