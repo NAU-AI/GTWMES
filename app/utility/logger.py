2dccacd9e4940d15d4c22aa58e9ff373d10b2eac
tdedevelopment <development@tde.pt>
2025-03-13 00:17:04 +0000

    MES-1013 - Refactor Service Layer for Business Logic Adaptation

--- app/utility/logger.py
+++ app/utility/logger.py
@@ -4,9 +4,11 @@ from logging.handlers import TimedRotatingFileHandler
 from datetime import datetime
 
 LOG_DIR = "logs"
-LOG_FILE = os.path.join(LOG_DIR, f"app.{datetime.now().strftime('%Y-%m-%d')}.log")
-
 os.makedirs(LOG_DIR, exist_ok=True)
+
+
+def get_log_filename():
+    return os.path.join(LOG_DIR, f"app.{datetime.now().strftime('%Y-%m-%d')}.log")
 
 
 class Logger:
@@ -22,14 +24,14 @@ class Logger:
 
         if not logger.handlers:
             file_handler = TimedRotatingFileHandler(
-                filename=LOG_FILE,
+                filename=get_log_filename(),
                 when="midnight",
                 interval=1,
                 backupCount=7,
                 encoding="utf-8",
                 utc=False,
             )
-            file_handler.suffix = "%Y-%m-%d"  # Not needed anymore, already in filename
+
             file_formatter = logging.Formatter(
                 "%(asctime)s - %(levelname)s - %(name)s - %(message)s",
                 datefmt="%Y-%m-%d %H:%M:%S",
